d51bd80058d6cbb40f5f30a5600636d6c449ccac
jisub0906 <jisub0906@gmail.com>
2025-04-15 13:04:59 +0900

    롤링배너 모바일,웹환경 최적화

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -12,7 +12,7 @@ export default function Header() {
             <Image 
               src="/logo.png" 
               alt="logo" 
-              width={350} 
+              width={330} 
               height={60} 
               priority
               quality={100}
@@ -26,7 +26,6 @@ export default function Header() {
         </div>
         <div className={styles.auth}>
           <Link href="/login">로그인</Link>
-          <span> / </span>
           <Link href="/signup">회원가입</Link>
         </div>
       </div>
